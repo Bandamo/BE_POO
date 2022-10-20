2944c866676757d433ff5ef58d55a858c4887f35
Bandamo <bandamo@live.fr>
2022-10-20 08:07:08 +0200

    First commit

---
+++ Part1.h
@@ -0,0 +1,51 @@
+#include <iostream>
+
+
+
+class Source{
+    public:
+        Source();
+        virtual float tension(float) = 0;
+    protected:
+        float V0;
+        float phi;
+};
+
+class Periodic:public Source{
+    public:
+        Periodic();
+    protected:
+        float P;
+
+};
+
+class Creneau:public Periodic{
+    public:
+        Creneau();
+        float tension(float);
+    protected:
+        float T;
+};
+
+class Triangulaire:public Periodic{
+    public:
+        float tension(float);
+};
+
+class Sinusoidal:public Periodic{
+    public:
+        float tension(float);
+};
+
+class Echelon:public Source{
+    public:
+        float tension(float);
+};
+
+class Rectangle:public Source{
+    public:
+        Rectangle();
+        float tension(float);
+    protected:
+        float T;
+};
